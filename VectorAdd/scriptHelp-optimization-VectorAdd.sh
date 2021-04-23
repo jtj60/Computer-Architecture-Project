0cf7fe1b5a1e1158eae0a02d82e0f3acbcd1dca3
garrett10101 <garrettdipalma2@gmail.com>
2021-04-22 20:55:39 -0500

    Added script for gcc and nvcc optimization levels

---
+++ VectorAdd/scriptHelp-optimization-VectorAdd.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+CFILE=VectorAdd.c
+CFILEEXEC=VectorAdd
+CUDAFILE=VectorAddCuda.cu
+CUDAFILEEXEC=VectorAddCuda
+
+#Optimization Level C File
+for num in {1..3}
+do
+echo "Optimization Level O$num"
+gcc -o $CFILEEXEC -O$num $CFILE -fopenmp
+echo garrett1 | sudo -S perf stat --event cache-references,cache-misses,cycles,page-faults,instructions,cpu-clock ./$CFILEEXEC
+done
+
+#Optimization Level Cuda File
+for num in {1..3}
+do
+echo "Optimization Level O$num"
+nvcc -o $CUDAFILEEXEC -O$num $CUDAFILE
+echo garrett1 | sudo -S nvprof --print-gpu-trace --print-api-trace ./$CUDAFILEEXEC
+done 
